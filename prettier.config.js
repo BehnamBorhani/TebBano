73c9970168761aa56c25ed48e8e32889f99f9674
Behnam <behnamborhani95@gmail.com>
2024-03-25 21:06:07 +0330

    adding prettier to format codes in one form

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+// prettier.config.js
+module.exports = {
+  config: {
+    trailingComma: "es5",
+    tabWidth: 4,
+    semi: false,
+    singleQuote: true,
+  },
+  plugins: ["prettier-plugin-tailwindcss"],
+};
