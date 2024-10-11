de7f9723358611630fd6e21fc0354d1472404326
Behnam <behnamborhani95@gmail.com>
2024-10-11 22:29:38 +0330

    [ADD] Testimonials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import { API_URL } from "@/configs/global";
 import { Doctor } from "./(doctors)/_types/doctor.model";
 import { homeFeatures } from "@/data/home-features";
 import HomeFeature from "./_component/home-feature/home-feature";
+import { TestimonialList } from "./_component/testimonial/testimonial-list";
+import { testimonials } from "@/data/testimonial";
 
 async function getِDoctors(count: number): Promise<[]> {
   const res = await fetch(
@@ -27,11 +29,11 @@ export default async function Home() {
       <section className="rounded-t-3xl bg-white-50 px-10 py-20 shadow-2xl md:rounded-t-5xl md:pt-20">
         <div className="container">
           <h2 className="mb-5 text-xl font-bold md:text-3xl">معرفی پزشکان</h2>
-          {/* <DoctorCardList doctors={doctorsData} /> */}
+          <DoctorCardList doctors={doctorsData} />
         </div>
       </section>
 
-      <section className="features mb-12 bg-third-50 py-12 lg:mb-36">
+      <section className="features z-10 bg-third-50 py-12">
         <div className="container">
           <div className="flex flex-wrap items-center justify-between gap-y-11 lg:gap-x-11">
             {homeFeatures.map((feature) => (
@@ -39,6 +41,20 @@ export default async function Home() {
             ))}
           </div>
         </div>
+      </section>
+
+      <section className="relative bg-white-50 pt-28">
+        {/* <div className="pointer-events-none absolute -top-52 bottom-0 left-1/2 aspect-square w-1/2 -translate-x-1/2 rounded-full bg-primary-300 opacity-30 blur-3xl"></div> */}
+        <h2 className="relative z-0 mx-auto block w-fit text-3xl font-extrabold text-icon">
+          <span className="absolute -right-3 -top-2 -z-10 inline-block h-8 w-8 rounded-full bg-icon-700 opacity-25"></span>
+          تجربه هم‌میسرهای تبانو
+        </h2>
+
+        <p className="mb-20 mt-2 text-center text-lg">
+          تو اینجا تنها نیستی. ببین هم‌مسیرهات نظرشون در مورد خدمات ما چیه
+        </p>
+        
+        <TestimonialList testimonials={testimonials} />
       </section>
     </div>
   );
